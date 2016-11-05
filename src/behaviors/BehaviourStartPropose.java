e92e9e5518dff4ec768df4e811a08c6839e50bad
Jony <jony.fes90@gmail.com>
2016-11-05 11:30:39 -0300

    cualquiera

--- src/behaviors/BehaviourStartPropose.java
+++ src/behaviors/BehaviourStartPropose.java
@@ -6,13 +6,10 @@ import agents.PeliVal;
 import core.Movie;
 import core.MovieOntology;
 import core.SeeMovie;
-<<<<<<< HEAD
-=======
 import jade.content.lang.Codec;
 import jade.content.lang.Codec.CodecException;
 import jade.content.onto.Ontology;
 import jade.content.onto.OntologyException;
->>>>>>> eb77d9178e9c5499cf24590ba55bfdb61e3e07c6
 import jade.core.AID;
 import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
@@ -48,34 +45,18 @@ public class BehaviourStartPropose extends Behaviour	{
 	       	 
 	       	 SeeMovie movies = new SeeMovie(mov);
 	       	 ACLMessage mensaje = new ACLMessage(ACLMessage.PROPOSE);
-<<<<<<< HEAD
 
-	       	 Movie mov = new Movie(coleccion.get(contador).getName());
-	       	 SeeMovie movies = new SeeMovie(mov);
-	       	 
-=======
->>>>>>> eb77d9178e9c5499cf24590ba55bfdb61e3e07c6
 	       	 mensaje.setLanguage(myAgent.getContentManager().getLanguageNames()[0]);
 	       	 mensaje.setOntology(MovieOntology.ONTOLOGY_NAME);
 	       	 mensaje.setSender(myAgent.getAID());
 	       	 mensaje.addReceiver(id);
 	       	 try {
-<<<<<<< HEAD
+
 	       		myAgent.getContentManager().fillContent(mensaje, movies);
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
 	       	myAgent.send(mensaje);
-
-=======
-				myAgent.getContentManager().fillContent(mensaje, movies);
-			} catch (CodecException | OntologyException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	       	 myAgent.send(mensaje);
-	       	 //Envio el mensaje
->>>>>>> eb77d9178e9c5499cf24590ba55bfdb61e3e07c6
 	       	 estado = 10;
 	       	 System.out.println(myAgent.getLocalName() + " envia una propuesta: "+mov.getMovie()+" al agente: " + id.getLocalName() + " ---- BehaviourStartPropose ");
 	       	 fin = true;
